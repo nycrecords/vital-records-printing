8b28ba59ad76dea78432e55bd961adb7f40561af
Vincent <vwong@records.nyc.gov>
2017-02-28 16:10:09 -0500

    search form and models WIP

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms.fields import StringField, SubmitField, SelectField, IntegerField
+from wtforms.validators import Length
 
 
 class SearchForm(FlaskForm):
-    name = StringField("name", validators=[Required()])
-    submit = SubmitField("submit")
+	type = SelectField("Type of Certificate", choices=[('b', 'Birth'),('d', 'Death'),('m', 'Marriage')])
+	county = SelectField("County", choices=[('k', 'Brooklyn'),('q', 'Queens'),('b', 'Bronx'),('m', 'Manhattan'),('r', 'Staten Island')])
+	year = IntegerField("Year")
+	number = IntegerField("Certificate Number")
+	first_name = StringField("First Name")
+	last_name = StringField("Last Name")
+	soundex = StringField("Soundex", validators=[Length(max=4)])
+	# name = StringField("name", validators=[Required()])
+	submit = SubmitField("submit")
--- config.py
+++ config.py
@@ -2,6 +2,8 @@
 class Config(object):
     DEBUG = True
     SECRET_KEY = "secrets"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://vital_records_printing_db@10.0.0.2:5432/vital_records_printing'  # TODO: vagrant->actual user
 
 
 class DevelopmentConfig(Config):
