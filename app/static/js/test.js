b48df8a9554204534eab4fce2c48adfdc0032e17
Jonathan Yu <jyu@records.nyc.gov>
2017-03-10 18:33:25 -0500

    initial commit

---
+++ app/static/js/test.js
@@ -0,0 +1,9 @@
+// Caman("#test", "./static/img/test_pic.jpg", function () {
+//   // manipulate image here
+//   this.brightness(40).render();
+// });
+
+
+function myFunction() {
+    document.getElementsByTagName("H1")[0].setAttribute("class", "democlass"); 
+}
