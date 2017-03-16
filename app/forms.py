0f45087282b118f8cb17c49276048bdd5128175e
Vincent Wong <vwong@records.nyc.gov>
2017-03-16 16:13:55 -0400

    forgot to add items last commit

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,17 @@ from wtforms.fields import StringField, SubmitField, SelectField, IntegerField
 from wtforms.validators import Length, NumberRange, Optional
 
 
+class SelectSortField(SelectField):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            *args,
+            choices=[('none', 'None'), ('asc', 'Ascending'), ('desc', 'Descending')],
+            **kwargs)
+
+
 class SearchForm(FlaskForm):
+    # visible inputs
     type = SelectField("Type of Certificate", choices=[('birth', 'Birth'), ('death', 'Death'), ('marriage', 'Marriage')])
     county = SelectField("County", choices=[('kings', 'Brooklyn'), ('queens', 'Queens'), ('bronx', 'Bronx'), ('manhattan', 'Manhattan'),
                                             ('richmond', 'Staten Island')])
@@ -12,5 +22,12 @@ class SearchForm(FlaskForm):
     first_name = StringField("First Name")
     last_name = StringField("Last Name")
     soundex = StringField("Soundex", validators=[Length(max=4)])
-    # name = StringField("name", validators=[Required()])
-    submit = SubmitField("submit")
+
+    submit = SubmitField("Submit")
+
+    # hidden inputs
+    year_sort = SelectSortField("Year")
+    number_sort = SelectSortField("Certificate Number")
+    first_name_sort = SelectSortField("First Name")
+    last_name_sort = SelectSortField("Last Name")
+    soundex_sort = SelectSortField("Soundex")
