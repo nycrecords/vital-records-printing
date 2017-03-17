ed2a43964d3ec76f6747da594dbda5b33ad03011
Jonathan Yu <jyu@records.nyc.gov>
2017-03-17 17:32:36 -0400

    updated variable names

--- app/utils.py
+++ app/utils.py
@@ -8,8 +8,8 @@ def pdf_to_png(input_file_path, output_file_path):
     :param input_file_path:
     :param output_file_path:
     """
-    directory_name = os.path.basename(input_file_path)
-    directory_name = os.path.splitext(directory_name)[0]
-    new_directory_path = output_file_path + directory_name
+    new_file_name = os.path.basename(input_file_path)
+    new_file_name = os.path.splitext(new_file_name)[0]
+    new_directory_path = output_file_path + new_file_name
     subprocess.call(['mkdir {}'.format(new_directory_path)], shell=True)
-    subprocess.call(['convert {} {}/{}.png'.format(input_file_path, new_directory_path, directory_name)], shell=True)
+    subprocess.call(['convert {} {}/{}.png'.format(input_file_path, new_directory_path, new_file_name)], shell=True)
