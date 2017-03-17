91f59eacf543705f0030fbab43b4e977b392a0df
Jonathan Yu <jyu@records.nyc.gov>
2017-03-17 13:27:55 -0400

    changed database config back to vagrant

--- config.py
+++ config.py
@@ -3,8 +3,7 @@ class Config(object):
     DEBUG = True
     SECRET_KEY = "secrets"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://vital_records_printing_db@10.0.0.2:5432/vital_records_printing'  # TODO: vagrant->actual user
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/vital_records_printing'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://vital_records_printing_db@10.0.0.2:5432/vital_records_printing'  # TODO: vagrant->actual user
 
 
 class DevelopmentConfig(Config):
