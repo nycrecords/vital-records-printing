6c15b38dba91b8f452b97073cdb3d6a619c35d2d
palisand <palisandratos@records.nyc.gov>
2017-04-07 14:31:45 -0400

    move pngs to static folder
    Former-commit-id: 3f04f31c7f35514e99d9bde2df7ad7ad52bf572c

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv.main import load_dotenv
+from flask import url_for
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -11,7 +12,13 @@ class Config(object):
     SECRET_KEY = "secrets"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = 'postgresql://vital_records_printing_db@10.0.0.2:5432/vital_records_printing'  # TODO: vagrant->actual user
-    PNGS_DIRECTORY = os.environ.get('PNGS_DIRECTORY') or os.path.join(BASEDIR, "pngs")
+
+    # location of certificate image directory from within static folder (default: "img/certificate")
+    CERT_IMAGE_STATIC_DIRECTORY = os.environ.get('CERT_IMAGE_STATIC_DIRECTORY') or \
+                                  os.path.join("img", "certificate")
+
+    # absolute path to mounted certificate image directory (".../app/static/img/certificate")
+    CERT_IMAGE_DIRECTORY = os.path.join(BASEDIR, "app", "static", CERT_IMAGE_STATIC_DIRECTORY)
 
 
 class DevelopmentConfig(Config):
