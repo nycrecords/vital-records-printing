3cc07116c4c4ce52d9a82909e18b411331303914
palisand <palisandratos@records.nyc.gov>
2017-04-13 14:47:46 -0400

    reduce number of indexes
    Former-commit-id: c45cdc19b2e8d694093a63554d3f72891c5c9457

--- manage.py
+++ manage.py
@@ -8,56 +8,35 @@ manager = Manager(app)
 
 
 @manager.command
-def create_certificate_indexes(check_existing=False):
+def create_certificate_indexes():
     """
-    Create db indexes for table 'certificate'.
-    
-    This should be run *after* table population and is expected to take a while to complete.
-    
-    Indexes are *almost* all the possible combinations of the certificate 
-    columns that correspond to searchable fields:
-        type, county, year, first_name, last_name, number, soundex
-    for a total of 64 indexes.
-    
-    Since 'type' is always included in the search query, it will always be part
-    of every combination:
-        type
-        type, county
-        type, year
-        ...
-        type, county, year
-        type, county, soundex
-        ...
-    
-    :param check_existing: Check for and skip existing indices
+    Create db indexes (7) for table 'certificate'.
+    This should be run *after* table population.
+    Existing indexes will not be recreated.
     """
-    if check_existing:
-        from sqlalchemy.engine import reflection
-        insp = reflection.Inspector.from_engine(db.engine)
+    from sqlalchemy.engine import reflection
+    insp = reflection.Inspector.from_engine(db.engine)
 
-    Col = namedtuple("Col", ["string", "attr"])
-    cols = {
-        Col("type", Cert.type),
-        Col("county", Cert.county),
-        Col("year", Cert.year),
-        Col("number", Cert.number),
-        Col("first", Cert.first_name),
-        Col("last", Cert.last_name),
-        Col("soundex", Cert.soundex),
-    }
-    count = 0
-    for length in range(1, len(cols) + 1):
-        for comb in combinations(cols, length):
-            if Col('type', Cert.type) in comb:
-                index_name = "idx_{}".format("_".join((col.string for col in comb)))
-                count += 1
-                if check_existing and set(col.attr.key for col in comb) in (
-                    set(index["column_names"]) for index in insp.get_indexes(Cert.__tablename__)
-                ):
-                    print("{}\tSKIPPING\t{}".format(count, index_name))
-                    continue
-                print("{}\t{}".format(count, index_name))
-                db.Index(index_name, *(col.attr for col in comb)).create(db.engine)
+    Index = namedtuple("Index", ["name_suffix", "attributes"])
+    indexes = (
+        Index("type_county_year", (Cert.type, Cert.county, Cert.year)),
+        Index("type_year", (Cert.type, Cert.year)),
+        Index("year", (Cert.year, )),
+        Index("county", (Cert.county, )),
+        Index("firstname", (Cert.first_name, )),
+        Index("lastname", (Cert.last_name, )),
+        Index("number", (Cert.number, )),
+        Index("soundex", (Cert.soundex, ))
+    )
+    for i, index in enumerate(indexes):
+        index_name = "idx_{}".format(index.name_suffix)
+        if set(attr.key for attr in index.attributes) in (
+            set(index["column_names"]) for index in insp.get_indexes(Cert.__tablename__)
+        ):
+            print("{}\tAlready Created, Skipping\t{}".format(i, index_name))
+            continue
+        print("{}\t{}".format(i, index_name))
+        db.Index(index_name, *index.attributes).create(db.engine)
 
 
 @manager.command
