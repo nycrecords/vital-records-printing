33862ccc6cecae1db26d6efbfea612d877c5c14e
Joel Castillo <joel.castillo@nyu.edu>
2017-04-24 20:42:01 +0000

    Use correct paths for death and marriage SQL Files
    Use the correct paths for the death and marriage records that need to be imported.
    Previously failed to import because script assumed all SQL files were in the birth folder.
    
    Former-commit-id: c4875e40ffa6216346dd54d1a34dd3999bdcb943

--- SQL/run.sh
+++ SQL/run.sh
@@ -2,10 +2,10 @@
 
 # create tables
 psql -d vital_records_printing -f birth/nyc_birth_index_create.sql
-psql -d vital_records_printing -f birth/nyc_death_index_create.sql
-psql -d vital_records_printing -f birth/nyc_marriages_index_create.sql
+psql -d vital_records_printing -f death/nyc_death_index_create.sql
+psql -d vital_records_printing -f marriage/nyc_marriages_index_create.sql
 
 # insert rows
 psql -d vital_records_printing -f birth/nyc_birth_index_add.sql
-psql -d vital_records_printing -f birth/nyc_death_index_add.sql
-psql -d vital_records_printing -f birth/nyc_marriages_index_add.sql
+psql -d vital_records_printing -f death/nyc_death_index_add.sql
+psql -d vital_records_printing -f marriage/nyc_marriages_index_add.sql
