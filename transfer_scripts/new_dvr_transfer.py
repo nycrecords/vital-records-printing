5b564ecbc81b1b36395a0f97579331783b967fb9
Jonathan Yu <jyu@records.nyc.gov>
2017-08-03 16:38:44 -0400

    new paths are now created using os.path.join instead of appeneding

--- transfer_scripts/new_dvr_transfer.py
+++ transfer_scripts/new_dvr_transfer.py
@@ -25,12 +25,11 @@ for cert_type in cert_types:
         remove_county = os.path.split(new_dvr_structure_path)
         new_dvr_structure_path = remove_county[0]
     new_dvr_structure_path = os.environ.get('NEW_DVR_BASE_DIR')
-print('New Default DVR structure created')
+print('New Default DVR structure created' + '\n')
 
+new_path = os.environ.get('NEW_DVR_BASE_DIR') + os.sep
 
-new_path = os.environ.get('NEW_DVR_BASE_DIR')
-
-for subdir, dirs, files in os.walk('/mnt/dvr/'):
+for subdir, dirs, files in os.walk(os.environ.get('CUR_DVR_BASE_DIR')):
     for file in files:
         filepath = subdir + os.sep + file
 
@@ -39,6 +38,8 @@ for subdir, dirs, files in os.walk('/mnt/dvr/'):
             filename = split_filepath[-1]
 
             split_filename = filename.split('-')
+            # to ensure all files have the format 'TYPE-COUNTY-YEAR-NUMBER.pdf'
+            # if it is not in the proper format, write it to a file for later processing
             if len(split_filename) != 4:
                 print('BAD FILENAME DETECTED')
                 print(filepath + '\n')
@@ -51,52 +52,52 @@ for subdir, dirs, files in os.walk('/mnt/dvr/'):
                 year = split_filename[2]
                 cert_num = split_filename[3]
 
-                # add os.path.join
                 if cert_type is 'B':
-                    new_path += 'Births/'
+                    new_path = os.path.join(new_path, 'Births') + os.sep
                 if cert_type is 'D':
-                    new_path += 'Deaths/'
+                    new_path = os.path.join(new_path, 'Deaths') + os.sep
                 if cert_type is 'M':
-                    new_path += 'Marriages/'
+                    new_path = os.path.join(new_path, 'Marriages') + os.sep
 
                 if county is 'K':
-                    new_path += 'Kings/'
+                    new_path = os.path.join(new_path, 'Kings') + os.sep
                 if county is 'Q':
-                    new_path += 'Queens/'
+                    new_path = os.path.join(new_path, 'Queens') + os.sep
                 if county is 'B':
-                    new_path += 'Bronx/'
+                    new_path = os.path.join(new_path, 'Bronx') + os.sep
                 if county is 'X':
-                    new_path += 'Bronx/'
+                    new_path = os.path.join(new_path, 'Bronx') + os.sep
                 if county is 'M':
-                    new_path += 'Manhattan/'
+                    new_path = os.path.join(new_path, 'Manhattan') + os.sep
                 if county is 'R':
-                    new_path += 'Richmond/'
+                    new_path = os.path.join(new_path, 'Richmond') + os.sep
                 if county is 'S':
-                    new_path += 'Richmond/'
+                    new_path = os.path.join(new_path, 'Richmond') + os.sep
 
-                new_path += year + "/"
+                new_path = os.path.join(new_path, year) + os.sep
 
                 try:
                     os.makedirs(new_path)
-                    print(new_path + " created")
+                    print(new_path + " created \n")
                 except FileExistsError:
                     # print("Directory: " + new_path + " already exists")
                     pass
 
                 original_dvr_path = filepath.replace(' ', '\ ')
                 print('Original: ' + original_dvr_path)
-                new_dvr_path = '/vagrant/transfer_scripts/' + new_path
+                new_dvr_path = os.environ.get('NEW_DVR_TARGET_DIR') + new_path
                 print('New: ' + new_dvr_path)
                 copy_command = 'cp ' + original_dvr_path + ' ' + new_dvr_path
                 print(copy_command + '\n')
 
-                # try:
-                #     os.system(copy_command)
-                # except Exception:
-                #     print('Something went wrong!')
+                try:
+                    os.system(copy_command)
+                except Exception:
+                    print('Something went wrong!')
 
-                new_path = 'DVR/'
+                new_path = os.environ.get('NEW_DVR_BASE_DIR') + os.sep
         else:
+            # write the paths of all non pdf files to a file for later processing
             print("NOT A PDF DETECTED")
             print(filepath + '\n')
             not_pdf_file.write(filepath)
